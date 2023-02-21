23de5213af427afbd36a5ea29f8e3ea7907601d3
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-02-21 19:58:46 +0300

    Tue

---
+++ ClassesAbstract.kt
@@ -0,0 +1,44 @@
+	abstract class Person(name: String) {
+
+    init {
+        println("My name is $name.")
+    }
+
+    fun displaySSN(ssn: Int) {
+        println("My SSN is $ssn.")
+    }
+
+    abstract fun displayJob(description: String)
+}
+class Teacher(name: String): Person(name) {
+
+    override fun displayJob(description: String) {
+        println(description)
+    }
+}
+fun main(args: Array<String>) {
+    val jack = Teacher("Jack Smith")
+    jack.displayJob("I'm a mathematics teacher.")
+    
+}
+
+abstract class Person(name: String) {
+
+    //Parameters 
+    ////member functions
+    abstract fun displayJob(description: String)
+}
+class Teacher(name: String): Person(name) {
+    //class that inherits
+
+    override fun displayJob(description: String) {
+        println(description)
+    }
+}
+fun main(args: Array<String>) {
+    //Object of the class 
+    ////accessing th function 
+    val jack = Teacher("Jack Smith")
+    jack.displayJob("I'm a mathematics teacher.")
+    
+}
